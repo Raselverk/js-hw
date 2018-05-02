92bc51e18da87c550b0806a86be5d8f4b838e56a
Ruslan <overko@windowslive.com>
2018-05-02 16:33:51 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -1,21 +1,20 @@
 const a = parseFloat(prompt('Enter a'));
 const b = parseFloat(prompt('Enter b'));
 const c = parseFloat(prompt('Enter c'));
-const discrim= b * b - 4 * a * c;
+const discrim = b * b - 4 * a * c;
 
-function quadrEq(a,b,c) {
+function quadrEq(a, b, c) {
     if (discrim > 0) {
-         
-        const x1 = (-b + Math.sqrt(discrim)) / 2 * a;
-        const x2 = (-b - Math.sqrt(discrim)) / 2 * a;
+        const x1 = (-b + Math.sqrt(discrim)) / (2 * a);
+        const x2 = (-b - Math.sqrt(discrim)) / (2 * a);
         return 'x1:' + x1 + '<br>' + 'x2:' + x2;
     }
     else if (discrim === 0) {
         const x = -(b / 2 * a);
         return 'x:' + x;
     }
-    else if (discrim < 0) {
-        return 'No solution equation';
-    }
+    (discrim < 0) 
+    return 'No solution equation';
+    
 }
-document.write(quadrEq(a,b,c));
+document.write(quadrEq(a, b, c));
