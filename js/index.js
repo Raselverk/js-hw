70700197dc83f69ebe2b5f7e9cc9e67ec649b4b6
Ruslan <overko@windowslive.com>
2018-05-02 12:33:02 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -1,22 +1,21 @@
-function quadrEq(){
-    
-
 const a = parseFloat(prompt('Enter a'));
 const b = parseFloat(prompt('Enter b'));
 const c = parseFloat(prompt('Enter c'));
-const discrim = b * b - 4 * a * c;
-   
-    if (discrim < 0) {
-        document.write('no solution');
+const discrim= b * b - 4 * a * c;
+
+function quadrEq(a,b,c) {
+    if (discrim > 0) {
+         
+        const x1 = (-b + Math.sqrt(discrim)) / 2 * a;
+        const x2 = (-b - Math.sqrt(discrim)) / 2 * a;
+        return 'x1:' + x1 + '<br>' + 'x2:' + x2;
     }
     else if (discrim === 0) {
-       const x = -(b / 2 * a);
-        document.write('x:' + x);
+        const x = -(b / 2 * a);
+        return 'x:' + x;
     }
-    else if(discrim > 0) {
-       const x1 = (-b + Math.sqrt(discrim())) / 2 * a;
-       const x2 = (-b - Math.sqrt(discrim())) / 2 * a;
-        document.write('x1:' + x1 + '<br>');
-        document.write('x2:' + x2);
+    else if (discrim < 0) {
+        return 'No solution equation';
     }
 }
+document.write(quadrEq(a,b,c));
